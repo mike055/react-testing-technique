29e8620ded37d22e837e8a62fe41006a3b4d146f
Michael Blackley <michael.blackley@rea-group.com>
2019-04-24 09:34:59 +1000

    more tests and loading overlay

--- src/components/to-do/View.tsx
+++ src/components/to-do/View.tsx
@@ -1,15 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Container } from './styles';
 
 import ToDoInput from './components/to-do-input';
 import ToDoList from './components/to-do-list';
+import Loading from '../../components/loading';
 
 import { ToDo } from './types';
 
 const View = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [todos, setTodos] = useState<ToDo[]>([]);
   const [lastTodoId, setLastTodoId] = useState(0);
   
+  useEffect(()=> {
+    setTimeout(()=> {
+      setIsLoading(false);
+    }, 1000);
+  },[]);
+
   const getNextTodoId = () => {
     const nextTodoId = lastTodoId + 1;
     setLastTodoId(nextTodoId);
@@ -61,11 +69,15 @@ const View = () => {
     setTodos(updatedTodos);
   };
 
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <Container>
+    <Container aria-label="Todos">
       <ToDoInput addTodo={ addTodo } />
       <ToDoList todos={todos} markTodoAsComplete={ markTodoAsComplete } removeTodo={ removeTodo } />
-    </Container>
+    </Container>      
   )
 };
 
--- src/components/to-do/index.test.tsx
+++ src/components/to-do/index.test.tsx
@@ -1,57 +1,81 @@
 import React from 'react';
-import { render, cleanup, fireEvent, RenderResult, getByLabelText } from 'react-testing-library';
+import { render, cleanup, fireEvent, RenderResult, getByLabelText, waitForElement } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 
 import Todos from './index';
 
 describe('todos', () => {
-  let renderedResult: RenderResult;
-  let renderedInput: HTMLElement;
-
-  beforeEach(()=> {
-    renderedResult = render(<Todos />);
-    renderedInput = getByLabelText(renderedResult.container, 'Enter a To Do:');
-  });
 
   afterEach(()=> {
     cleanup();
   });
 
-  const addTodo = (text: string) => {
+  const addTodo = (renderedResult: RenderResult, text: string) => {
+    const renderedInput = getByLabelText(renderedResult.container, 'Enter a To Do:');
     fireEvent.change(renderedInput, { target: { value:  text } });
     fireEvent.keyDown(renderedInput, { keyCode: 13 });
-  }
+  };
 
-  const assertTodoInList = (todoText: string) => {
+  const assertTodoInList = (renderedResult: RenderResult, todoText: string) => {
     const { queryByText } = renderedResult;
     expect(queryByText(todoText)).toBeInTheDocument();
-  }
+  };
 
-  const assertTodoNotInList = (todoText: string) => {
+  const assertTodoNotInList = (renderedResult: RenderResult, todoText: string) => {
     const { queryByText } = renderedResult;
     expect(queryByText(todoText)).not.toBeInTheDocument();
-  }
+  };
+
+  describe('when component loaded', () => {
+
+    describe('when loading is complete', () => {
+      it('the loading overlay is shown and then removed', async () => {
+        const renderedResult = render(<Todos />);
+        const { getByLabelText, queryByLabelText } = renderedResult;
+
+        expect(queryByLabelText('Loading')).toBeInTheDocument();
+        expect(queryByLabelText('Todos')).not.toBeInTheDocument();
+
+        await waitForElement(() =>
+          getByLabelText('Todos'),
+        );
+
+        expect(queryByLabelText('Loading')).not.toBeInTheDocument();
+      });
+    });
+  });
 
   describe('when adding a todo', () => {
     const firstTodo = "This is a test TODO!";
 
-    beforeEach(()=>{
-      addTodo(firstTodo);
-    });
+    it('adds it to the list', async () => {
+      const renderedResult = render(<Todos />);
+      const { getByLabelText } = renderedResult;
 
-    it('adds it to the list', () => {
-      assertTodoInList(firstTodo);
+      await waitForElement(() =>
+        getByLabelText('Todos'),
+      );
+
+      addTodo(renderedResult, firstTodo);
+
+      assertTodoInList(renderedResult, firstTodo);
     });
 
     describe('when adding a second todo', () => {
+
       const secondTodo = "Second Todo";
   
-      beforeEach(()=>{
-        addTodo(secondTodo);
-      });
+      it('adds it to the top of the list', async () => {
+        const renderedResult = render(<Todos />);
+        const { getByLabelText, getByTestId } = renderedResult;
   
-      it('adds it to the top of the list', () => {
-        const { getByTestId } = renderedResult;
+        await waitForElement(() =>
+          getByLabelText('Todos'),
+        );
+
+        addTodo(renderedResult, firstTodo);
+        addTodo(renderedResult, secondTodo);
+
         const todoList = getByTestId('todo-list');
 
         expect(todoList.childNodes[0].textContent).toBe(secondTodo);
@@ -64,16 +88,19 @@ describe('todos', () => {
     const todo1 = "Todo number 1";
     const todo2 = "Todo number 2";
     const todo3 = "Todo number 3";
-    
-    beforeEach(()=>{
-      addTodo(todo1);
-      addTodo(todo2);
-      addTodo(todo3);
-    });
 
-    it('it moves to the bottom of the list', () => {
+    it('it moves to the bottom of the list', async () => {
+      const renderedResult = render(<Todos />);
+      const { getByLabelText, getByTestId } = renderedResult;
 
-      const { getByTestId } = renderedResult;
+      await waitForElement(() =>
+        getByLabelText('Todos'),
+      );
+
+      addTodo(renderedResult, todo1);
+      addTodo(renderedResult, todo2);
+      addTodo(renderedResult, todo3);
+
       const todoList = getByTestId('todo-list');
 
       expect(todoList.childNodes[0].textContent).toBe(todo3);
@@ -93,20 +120,24 @@ describe('todos', () => {
     const todo1 = "Todo number 1";
     const todo2 = "Todo number 2";
     
-    beforeEach(()=>{
-      addTodo(todo1);
-      addTodo(todo2);
-    });
+    it('it is removed from the list', async () => {
+      const renderedResult = render(<Todos />);
+      const { getByLabelText, getByTestId } = renderedResult;
 
-    it('it is removed from the list', () => {
-      assertTodoInList(todo1);
-      assertTodoInList(todo2);
+      await waitForElement(() =>
+        getByLabelText('Todos'),
+      );
 
-      const { getByLabelText } = renderedResult;
+      addTodo(renderedResult, todo1);
+      addTodo(renderedResult, todo2);
+
+      assertTodoInList(renderedResult, todo1);
+      assertTodoInList(renderedResult, todo2);
+
       fireEvent.click(getByLabelText('Remove ' + todo2));
 
-      assertTodoInList(todo1);
-      assertTodoNotInList(todo2);
+      assertTodoInList(renderedResult, todo1);
+      assertTodoNotInList(renderedResult, todo2);
     });
   });
 });
